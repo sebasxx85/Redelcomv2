999b77a5905e3868c402580fcd796cd5f498a078
sebaxx85 <isurus.1313@gmail.com>
2024-06-26 03:37:19 -0400

    act 15 to 16

--- src/app/guards/form-reg.guard.ts
+++ src/app/guards/form-reg.guard.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivateChild, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { LoginService } from '../services/login.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class FormRegGuard implements CanActivateChild {
+export class FormRegGuard  {
 
   constructor(
     private loginService: LoginService
--- src/app/guards/login.guard.ts
+++ src/app/guards/login.guard.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, CanDeactivate, CanLoad, CanMatch, Route, Router, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, Route, Router, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { LoginService } from '../services/login.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class LoginGuard implements CanActivate {
+export class LoginGuard  {
 
   constructor(
     private router: Router,
